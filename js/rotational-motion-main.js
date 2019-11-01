6aaf2f287559e945f796e3d7ec75c2c0aace0e1e
Brandon Li <brandon.li820@icloud.com>
2019-10-31 20:51:46 -0600

    add eslint config

---
+++ js/rotational-motion-main.js
@@ -0,0 +1,16 @@
+// Copyright © 2019 Brandon Li. All rights reserved.
+
+/**
+ * Entry point for the sim.
+ *
+ * @author Brandon Li <brandon.li820@gmail.com>
+ */
+
+define( require => {
+  'use strict';
+
+  // modules
+  const assert = require( 'SIM_CORE/util/assert' );
+
+  console.log( assert )
+} );
