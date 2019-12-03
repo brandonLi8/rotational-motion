09a1ac2951101e291708d023ec02bd9c012ae4a2
Brandon Li <brandon.li820@icloud.com>
2019-12-02 22:03:41 -0700

    Add Intro Screen Module

---
+++ js/intro/IntroScreen.js
@@ -0,0 +1,35 @@
+// Copyright © 2019 Brandon Li. All rights reserved.
+
+/**
+ * The 'Intro' screen. Conforms to the contract specified in sim-core/Screen.
+ *
+ * @author Brandon Li <brandon.li820@gmail.com>
+ */
+
+define( require => {
+  'use strict';
+
+  // modules
+  const IntroScreenView = require( 'ROTATIONAL_MOTION/intro/IntroScreenView' );
+  const Screen = require( 'SIM_CORE/Screen' );
+
+  // constants
+  const INTRO_SCREEN_NAME = 'Intro';
+
+  class IntroScreen extends Screen {
+
+    /**
+     * @param {Tandem} tandem
+     */
+    constructor( tandem ) {
+
+      const options = {
+        name: INTRO_SCREEN_NAME
+      };
+
+      super( () => 5, () => new IntroScreenView(), options );
+    }
+  }
+
+  return IntroScreen
+} );
