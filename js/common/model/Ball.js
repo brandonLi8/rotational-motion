c666b1cd00d4994e7180a7658917a00bb26d4085
Brandon Li <brandon.li820@icloud.com>
2019-12-09 19:18:11 -0700

    add skeleton of ball node and remove velocity functionality of ball

--- js/common/model/Ball.js
+++ js/common/model/Ball.js
@@ -6,11 +6,10 @@
  *
  * Primary responsibilities are:
  *  1. position Property to track the position of the Ball's center.
- *  2. velocity Property to track the linear velocity of the center of mass.
- *  3. keep track of other simple ball properties (radius, ranges, etc.)
- *  4. Abstract step method. This is done because the step functionality differs
+ *  2. radius Property to track the radius of the Ball
+ *  3. Abstract step method. This is done because the step functionality differs
  *     for different screens (rotating about center vs rotating around a spinner).
- *  5. Abstract drag method (called when the Ball is dragged). This is done because
+ *  4. Abstract drag method (called when the Ball is dragged). This is done because
  *     dragging and its bounds differs for different screens.
  *
  * @author Brandon Li <brandon.li820@gmail.com>
@@ -32,7 +31,7 @@ define( require => {
      * @param {Vector} initialCenterPosition - starting center position for the Ball
      * @param {Object} [options] - key-value pairs that control simple ball properties.
      */
-    constructor( initialCenterPosition options ) {
+    constructor( initialCenterPosition, options ) {
 
       assert( initialCenterPosition instanceof Vector, `invalid initialCenterPosition: ${ initialCenterPosition }` );
       assert( !options || Object.getPrototypeOf( options ) === Object.prototype, `invalid options: ${ options }` );
@@ -43,14 +42,14 @@ define( require => {
         isDraggable: true,           // {boolean} - indicates if the user can drag this Ball. If true, sub classes must
                                      //             override the dragTo() abstract method.
 
-        startingVelocity: 0,         // {number} - the default velocity of the center of mass, in meters per second
-        velocityRange: Vector.ZERO,  // {Vector} - the range of the velocity of the center of mass, in meters per second
-
         startingRadius: 0.05,        // {number} - the default radius of Ball, in meters
 
         // rewrite options such that it overrides the defaults above if provided.
         ...options
       };
+
+      // @public (read-only) isDraggable - indicates if the user can drag this Ball. See `dragTo()` abstract method.
+      this.isDraggable = options.isDraggable;
 
       //----------------------------------------------------------------------------------------
       // Responsibility 1: create the position Property to track the position of the Ball's center
@@ -59,22 +58,7 @@ define( require => {
       this.centerPositionProperty = new Property( initialCenterPosition, { type: Vector } );
 
       //----------------------------------------------------------------------------------------
-      // Responsibility 2: create a velocity Property to track the linear velocity of the center of mass (m/s).
-
-      // @public (read-only) velocityRange - Range of the linear velocity of the center of mass of the ball.
-      this.velocityRange = options.velocityRange;
-
-      // @public (read-only) velocityProperty - Property of the linear velocity of the center of mass of the Ball.
-      this.velocityProperty = new Property( options.startingVelocity, {
-        type: 'number',
-        isValidValue: value => value >= this.velocityRange.x && value <= this.velocityRange.y
-      } );
-
-      //----------------------------------------------------------------------------------------
-      // Responsibility 3: keep track of other simple ball properties (radius, ranges, etc.)
-
-      // @public (read-only) isDraggable - indicates if the user can drag this Ball. See `dragTo()` abstract method.
-      this.isDraggable = options.isDraggable;
+      // Responsibility 2: create the radius Property to track the radius of the Ball
 
       // @public (read-only) radiusProperty - Property of the radius of the Ball.
       this.radiusProperty = new Property( options.startingRadius, {
@@ -101,6 +85,15 @@ define( require => {
      * @param {Vector} position - the position of the Center of the Ball
      */
     dragTo( position ) { assert( false, `abstract dragTo class must be overriden` ); }
+
+    /**
+     * Resets the Ball.
+     * @public
+     */
+    reset() {
+      this.centerPositionProperty.reset();
+      this.radiusProperty.reset();
+    }
 
     //----------------------------------------------------------------------------------------
     // Convenience Methods
@@ -138,23 +131,6 @@ define( require => {
     set radius( radius ) {
       assert( typeof radius === 'number' && radius >= 0, `invalid radius: ${ radius }` );
       this.radiusProperty.value = radus;
-    }
-
-    /**
-     * Gets the Ball's center of mass velocity, in meters per second.
-     * @public
-     * @returns {number} - in meters per second
-     */
-    get velocity() { return this.velocityProperty.value; }
-
-    /**
-     * Sets the Ball's velocity, in meters per second.
-     * @public
-     * @param {number} velocity, in meters per second.
-     */
-    set velocity( velocity ) {
-      assert( velocity instanceof Vector, `invalid velocity: ${ velocity }` );
-      this.velocityProperty.value = center;
     }
   }
 
