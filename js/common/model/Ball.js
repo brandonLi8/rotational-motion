2a892c1681cafc6ea07ca6c54b5e17354b9a3f64
Brandon Li <brandon.li820@icloud.com>
2019-12-09 17:49:48 -0700

    improve ball model

--- js/common/model/Ball.js
+++ js/common/model/Ball.js
@@ -4,12 +4,14 @@
  * Ball is the root model encapsulation for a Ball.
  * It is abstract and intended to be subclassed as its functionality differs for different screens.
  *
- * Responsibilities of this class are:
+ * Primary responsibilities are:
  *  1. position Property to track the position of the Ball's center.
  *  2. velocity Property to track the linear velocity of the center of mass.
- *  3. keep track of other simple ball properties (radius, ranges, ...)
+ *  3. keep track of other simple ball properties (radius, ranges, etc.)
  *  4. Abstract step method. This is done because the step functionality differs
  *     for different screens (rotating about center vs rotating around a spinner).
+ *  5. Abstract drag method (called when the Ball is dragged). This is done because
+ *     dragging and its bounds differs for different screens.
  *
  * @author Brandon Li <brandon.li820@gmail.com>
  */
@@ -24,7 +26,6 @@ define( require => {
   const Vector = require( 'SIM_CORE/util/Vector' );
   const Util = require( 'SIM_CORE/util/Util' );
 
-
   class Ball {
 
     /**
@@ -37,30 +38,49 @@ define( require => {
       assert( !options || Object.getPrototypeOf( options ) === Object.prototype, `invalid options: ${ options }` );
 
       //----------------------------------------------------------------------------------------
-      // Create the Velocity Properties
+      options = {
+
+        isDraggable: true,           // {boolean} - indicates if the user can drag this Ball. If true, sub classes must
+                                     //             override the dragTo() abstract method.
+
+        startingVelocity: 0,         // {number} - the default velocity of the center of mass, in meters per second
+        velocityRange: Vector.ZERO,  // {Vector} - the range of the velocity of the center of mass, in meters per second
+
+        startingRadius: 0.05,        // {number} - the default radius of Ball, in meters
+
+        // rewrite options such that it overrides the defaults above if provided.
+        ...options
+      };
+
+      //----------------------------------------------------------------------------------------
+      // Responsibility 1: create the position Property to track the position of the Ball's center
+
+      // @public (read-only) centerPositionProperty - Property of the position of the Ball's center
+      this.centerPositionProperty = new Property( initialCenterPosition, { type: Vector } );
+
+      //----------------------------------------------------------------------------------------
+      // Responsibility 2: create a velocity Property to track the linear velocity of the center of mass (m/s).
 
       // @public (read-only) velocityRange - Range of the linear velocity of the center of mass of the ball.
       this.velocityRange = options.velocityRange;
 
       // @public (read-only) velocityProperty - Property of the linear velocity of the center of mass of the Ball.
-      this.velocityProperty = new Property( options.startingVelocity, { type: 'number' } );
-
+      this.velocityProperty = new Property( options.startingVelocity, {
+        type: 'number',
+        isValidValue: value => value >= this.velocityRange.x && value <= this.velocityRange.y
+      } );
 
       //----------------------------------------------------------------------------------------
-      // Create the Radius Properties
+      // Responsibility 3: keep track of other simple ball properties (radius, ranges, etc.)
 
-      // @public (read-only) radiusRange - Range of the Ball's radius.
-      this.radiusRange = options.radiusRange;
+      // @public (read-only) isDraggable - indicates if the user can drag this Ball. See `dragTo()` abstract method.
+      this.isDraggable = options.isDraggable;
 
       // @public (read-only) radiusProperty - Property of the radius of the Ball.
       this.radiusProperty = new Property( options.startingRadius, {
         type: 'number',
         isValidValue: value => value >= 0 // radius must be greater than 0
       } );
-
-      //----------------------------------------------------------------------------------------
-      // @public (read-only) centerPositionProperty - Property of the position of the Ball's center
-      this.centerPositionProperty = new Property( initialCenterPosition, { type: Vector } );
     }
 
     /**
@@ -71,6 +91,16 @@ define( require => {
      * @param {number} dt - time in seconds
      */
     step( dt ) { assert( false, `abstract step class must be overriden` ); }
+
+    /**
+     * @abstract
+     * Called when the Ball is dragged ONLY if this.isDraggable is true. This is done because
+     * dragging bounds differs for different screens.
+     * @public
+     *
+     * @param {Vector} position - the position of the Center of the Ball
+     */
+    dragTo( position ) { assert( false, `abstract dragTo class must be overriden` ); }
 
     //----------------------------------------------------------------------------------------
     // Convenience Methods
