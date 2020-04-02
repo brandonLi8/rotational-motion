5f8e09405e652463ae67f34cfd8ff5aa74b69749
Brandon Li <brandon.li820@icloud.com>
2020-04-02 17:16:33 -0600

    #5 clean up Intro Ball

--- js/intro/model/IntroBall.js
+++ js/intro/model/IntroBall.js
@@ -49,10 +49,8 @@ define( require => {
 
       //----------------------------------------------------------------------------------------
 
-      // @public (read-only) {Spinner} - reference the spinner passed in
-      this.spinner = spinner;
-
-      //----------------------------------------------------------------------------------------
+      // @private {Spinner} - reference the spinner that was passed-in.
+      this._spinner = spinner;
 
       // @public (read-only) velocityProperty - Property of the linear (tangential) velocity of the center of mass of
       //                                        the Ball. Link lasts for the entire sim and is never disposed.
@@ -60,7 +58,7 @@ define( require => {
         spinner.angularVelocityProperty,  // in radians per second
         spinner.radiusProperty            // in meters
       ], ( angularVelocity, radius ) => {
-          // To calculate the velocity (in meters per second), multiply radius * omega
+          // To calculate the velocity (in meters per second), multiply radius * omega.
           // For more details on this calculation, see https://en.wikipedia.org/wiki/Angular_velocity.
           return angularVelocity * radius;
         } );
@@ -68,85 +66,43 @@ define( require => {
       // @public (read-only) accelerationProperty - Property of the linear (tangential) acceleration of the center of
       //                                            mass of the Ball. Link lasts for the entire sim and never disposed.
       this.accelerationProperty = new DerivedProperty( [
-        spinner.angularAccelerationProperty,  // in radians per second
-        spinner.radiusProperty            // in meters
+        spinner.angularAccelerationProperty,  // in radians per second per second
+        spinner.radiusProperty                // in meters
       ], ( angularAcceleration, radius ) => {
-          // To calculate the acceleration (in meters per second per second), multiply radius * alpha
+          // To calculate the acceleration (in meters per second per second), multiply radius * alpha.
           // For more details on this calculation, see https://en.wikipedia.org/wiki/Angular_acceleration
           return angularAcceleration * radius;
       } );
     }
 
     /**
-     * @abstract
-     * Steps the Ball by one time step. For this screen, the Ball spins around the origin (circular motion).
-     * This method changes the Ball's position such that it matches the correct angular acceleration,
-     * angular velocity, and radius.
-     *
-     * NOTE: this assumes that this is only called when the sim is playing.
-     * @public
-     *
-     * @param {number} dt - time in seconds
-     */
-    step( dt ) {
-      // Calculate the change in angle (in radians) based on the average angular velocity (rad/sec)
-      // This is calculated with the equation of kinematic: theta = initial-theta + omega * t + 1/2 * alpha * t^2
-      // Rearranging this equation and we get deltaTheta = omega * dt + 0.5 * alpha * t^2.
-      // For more info, see https://courses.lumenlearning.com/physics/chapter/10-2-kinematics-of-rotational-motion/
-      const deltaTheta = this.spinner.angularVelocity * dt + 0.5 * this.spinner.angularAcceleration * dt * dt;
-      this.spinner.angle += deltaTheta;
-    }
-
-    /**
-     * @abstract
      * @override
      * Called when the Ball is dragged. Passes a new attempted position to the Spinner, which will determine
      * new radii and constrain the dragging bounds.
+     *
+     * This is done for performance reasons, as setting the location and the having the Spinner set the location again
+     * to account for bounds restraints becomes slow.
      * @public
      *
      * @param {Vector} position - the position of the Center of the Ball
      */
-    dragTo( position ) {
-      this.spinner.dragBallTo( position );
-    }
-
-    //----------------------------------------------------------------------------------------
-    // Convenience Methods
-    //----------------------------------------------------------------------------------------
+    dragTo( position ) { this._spinner.dragBallTo( position ); }
 
     /**
-     * Gets the Ball's center of mass velocity, in meters per second.
+     * Gets the Ball's center of mass linear velocity, in meters per second.
      * @public
+     *
      * @returns {number} - in meters per second.
      */
     get velocity() { return this.velocityProperty.value; }
 
     /**
-     * Sets the Ball's velocity, in meters per second.
+     * Gets the Ball's center of mass linear acceleration, in meters per second per second.
      * @public
-     * @param {number} velocity, in meters per second.
-     */
-    set velocity( velocity ) {
-      assert( typeof velocity === 'number', `invalid velocity: ${ velocity }` );
-      this.velocityProperty.value = velocity;
-    }
-
-    /**
-     * Gets the Ball's center of mass acceleration, in meters per second per second.
-     * @public
+     *
      * @returns {number} - in meters per second per second.
      */
     get acceleration() { return this.accelerationProperty.value; }
-
-    /**
-     * Sets the Ball's acceleration, in meters per second per second.
-     * @public
-     * @param {number} acceleration, in meters per second per second.
-     */
-    set acceleration( acceleration ) {
-      assert( typeof acceleration === 'number', `invalid acceleration: ${ acceleration }` );
-      this.accelerationProperty.value = acceleration;
-    }
   }
 
   return IntroBall;
