30633b3671f0e947b5b93c86649c18d214a1d11b
Brandon Li <brandon.li820@icloud.com>
2020-04-01 18:28:16 -0600

    #5 clean up and change api of Ball.js model

--- js/common/model/Ball.js
+++ js/common/model/Ball.js
@@ -1,16 +1,16 @@
 // Copyright © 2019-2020 Brandon Li. All rights reserved.
 
 /**
- * Ball is the root model encapsulation for a Ball.
+ * Ball is the root model encapsulation for a Ball in all Screens.
  * It is abstract and intended to be subclassed as its functionality differs for different screens.
  *
  * Primary responsibilities are:
  *  1. position Property to track the position of the Ball's center.
- *  2. radius Property to track the radius of the Ball
- *  3. Abstract step method. This is done because the step functionality differs
- *     for different screens (rotating about center vs rotating around a spinner).
- *  4. Abstract drag method (called when the Ball is dragged). This is done because
- *     dragging and its bounds differs for different screens.
+ *  2. radius Property to track the inner radius of the Ball.
+ *  3. Abstract step method. This is done because the step functionality differs for different screens (rotates about
+ *     its center in the Rolling screen but rotates around a point outside the ball in the Intro screen).
+ *  4. Abstract dragTo method (called when the Ball is dragged). This is done because
+ *     dragging and its bounds differs for different screens. See sub-classes for more documentation.
  *
  * @author Brandon Li <brandon.li820@gmail.com>
  */
@@ -23,40 +23,35 @@ define( require => {
   const Property = require( 'SIM_CORE/util/Property' );
   const Vector = require( 'SIM_CORE/util/Vector' );
 
+  // @abstract
   class Ball {
 
     /**
      * @param {Vector} initialCenterPosition - starting center position for the Ball
+     * @param {number} initialRadius - the initial inner radius of the Ball.
      * @param {Object} [options] - key-value pairs that control simple ball properties.
      */
-    constructor( initialCenterPosition, options ) {
-
+    constructor( initialCenterPosition, initialRadius, options ) {
       assert( initialCenterPosition instanceof Vector, `invalid initialCenterPosition: ${ initialCenterPosition }` );
+      assert( typeof initialRadius === 'number', `invalid initialRadius: ${ initialRadius }` );
       assert( !options || Object.getPrototypeOf( options ) === Object.prototype, `invalid options: ${ options }` );
 
-      //----------------------------------------------------------------------------------------
       options = {
 
-        isDraggable: false,          // {boolean} - indicates if the user can drag this Ball. If true, sub classes must
-                                     //             override the dragTo() abstract method.
-
-        startingRadius: 0.05,        // {number} - the default radius of Ball, in meters
+        isDraggable: false, // {boolean} - indicates if the user can drag this Ball. If true, sub classes must
+                            //             override the dragTo() abstract method.
 
         // rewrite options such that it overrides the defaults above if provided.
         ...options
       };
 
+      //----------------------------------------------------------------------------------------
+
       // @public (read-only) isDraggable - indicates if the user can drag this Ball. See `dragTo()` abstract method.
       this.isDraggable = options.isDraggable;
 
-      //----------------------------------------------------------------------------------------
-      // Responsibility 1: create the position Property to track the position of the Ball's center
-
       // @public (read-only) centerPositionProperty - Property of the position of the Ball's center
       this.centerPositionProperty = new Property( initialCenterPosition, { type: Vector } );
-
-      //----------------------------------------------------------------------------------------
-      // Responsibility 2: create the radius Property to track the radius of the Ball
 
       // @public (read-only) radiusProperty - Property of the radius of the Ball.
       this.radiusProperty = new Property( options.startingRadius, {
@@ -67,7 +62,8 @@ define( require => {
 
     /**
      * @abstract
-     * Steps the Ball by one time step. This functionality differs for different screens.
+     * Steps the Ball by one time step. This functionality differs for different screens and is, thus, abstract.
+     * See sub-classes for specific documentation..
      * @public
      *
      * @param {number} dt - time in seconds
@@ -80,12 +76,12 @@ define( require => {
      * dragging bounds differs for different screens.
      * @public
      *
-     * @param {Vector} position - the position of the Center of the Ball
+     * @param {Vector} position - the position of the Center of the Ball to move the Ball to.
      */
     dragTo( position ) { assert( false, 'abstract dragTo class must be overridden' ); }
 
     /**
-     * Resets the Ball.
+     * Resets the Ball and its Properties.
      * @public
      */
     reset() {
@@ -109,10 +105,7 @@ define( require => {
      * @public
      * @param {Vector} center - in meter coordinates
      */
-    set center( center ) {
-      assert( center instanceof Vector, `invalid center: ${ center }` );
-      this.centerPositionProperty.value = center;
-    }
+    set center( center ) { this.centerPositionProperty.value = center; }
 
     /**
      * Gets the Ball's radius, in meters.
@@ -126,10 +119,7 @@ define( require => {
      * @public
      * @param {number} radius - in meters
      */
-    set radius( radius ) {
-      assert( typeof radius === 'number' && radius >= 0, `invalid radius: ${ radius }` );
-      this.radiusProperty.value = radius;
-    }
+    set radius( radius ) { this.radiusProperty.value = radius; }
   }
 
   return Ball;
