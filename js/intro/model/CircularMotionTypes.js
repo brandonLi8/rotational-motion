f6903e1da0b9fba87ac03df79dd202f33d7010e2
Brandon Li <brandon.li820@icloud.com>
2020-04-03 16:33:37 -0600

    #5 add CircularMotiontypes enum

---
+++ js/intro/model/CircularMotionTypes.js
@@ -0,0 +1,27 @@
+// Copyright 2019, University of Colorado Boulder
+
+/**
+ * Enumeration of the different 'types' of circular motion in the 'Intro' screen.
+ *
+ * @author Brandon Li
+ */
+
+define( require => {
+  'use strict';
+
+  // modules
+  const Enum = require( 'SIM_CORE/util/Enum' );
+
+  const CircularMotionTypes = new Enum( [
+
+    // The first 'scene' of the 'Intro' screen, where the angular acceleration (alpha) is always 0 and the user can
+    // change the angular speed (omega) and radius of the circular motion.
+    'UNIFORM',
+
+    // The second 'scene' of the 'Intro' screen, where the user can change the angular acceleration (alpha) of
+    // the circular motion.
+    'NON_UNIFORM'
+  ] );
+
+  return CircularMotionTypes;
+} );
