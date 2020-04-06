12c078cfc2a686f992dd6952412a2adafe5467b8
Brandon Li <brandon.li820@icloud.com>
2020-04-06 17:58:18 -0600

    #5 number display bug fixes

--- js/common/view/NumberDisplay.js
+++ js/common/view/NumberDisplay.js
@@ -18,14 +18,15 @@ define( require => {
   'use strict';
 
   // modules
-  const Rectangle = require( 'SIM_CORE/scenery/Rectangle' );
-  const Text = require( 'SIM_CORE/scenery/Text' );
+  const assert = require( 'SIM_CORE/util/assert' );
   const Node = require( 'SIM_CORE/scenery/Node' );
   const Property = require( 'SIM_CORE/util/Property' );
   const Range = require( 'SIM_CORE/util/Range' );
+  const Rectangle = require( 'SIM_CORE/scenery/Rectangle' );
+  const Symbols = require( 'SIM_CORE/util/Symbols' );
+  const Text = require( 'SIM_CORE/scenery/Text' );
   const Util = require( 'SIM_CORE/util/Util' );
   const Vector = require( 'SIM_CORE/util/Vector' );
-  const Symbols = require( 'SIM_CORE/util/Symbols' );
 
   class NumberDisplay extends Node {
 
@@ -46,8 +47,8 @@ define( require => {
         yAlign: 'center',  // {string} - the alignment of the Text: 'top', 'center', or 'bottom'.
         unit: null,        // {string} - if provided, this will be appended to the end of the Text as a unit.
         decimalPlaces: 0,  // {number|null} the number of decimal places to show. If null, the full value is displayed.
-        xMargin: 8,        // {number} - the x-margin between the longest/tallest Text and the background.
-        yMargin: 2,        // {number} - the x-margin between the longest/tallest Text and the background.
+        xMargin: 11,       // {number} - the x-margin between the longest/tallest Text and the background.
+        yMargin: 5,        // {number} - the x-margin between the longest/tallest Text and the background.
         cornerRadius: 0,   // {number} - the corner radius of the background
 
         backgroundFill: 'white',        // {string|Gradient} the fill of the background
@@ -74,23 +75,21 @@ define( require => {
       this._decminalPlaces = options.decimalPlaces;
 
       // Determine the widest value first.
-      const minValueString = this._formatValue( range.min );
-      const maxValueString = this._formatValue( range.max );
+      const minValueString = `${ range.min.toFixed( this._decminalPlaces ) } ${ this._unit || '' }`;
+      const maxValueString = `${ range.min.toFixed( this._decminalPlaces ) } ${ this._unit || '' }`;
       const longestValueString = minValueString.length > maxValueString.length ? minValueString : maxValueString;
 
       // @private {Text} - create the value Node which displays the Text of the number Property.
       this._valueNode = new Text( longestValueString, options.textOptions );
-      this._valueNode.maxWidth = this._valueNode.width;
 
       // @private {Rectangle} - create the Rectangle background Node
-      this._background = new Rectangle( 0, 0,
-        this.valueNode.width + 2 * options.xMargin,
-        this.valueNode.height + 2 * options.yMargin, {
-          cornerRadius: options.cornerRadius,
-          fill: options.backgroundFill,
-          stroke: options.backgroundStroke,
-          strokeWidth: options.backgroundStrokeWidth
-        } );
+      this._background = new Rectangle( this._valueNode.width + 2 * options.xMargin,
+                                        this._valueNode.height + 2 * options.yMargin, {
+        cornerRadius: options.cornerRadius,
+        fill: options.backgroundFill,
+        stroke: options.backgroundStroke,
+        strokeWidth: options.backgroundStrokeWidth
+      } );
 
       // Add the content of the Number Display as children
       this.children = [ this._background, this._valueNode ];
@@ -98,6 +97,9 @@ define( require => {
       // @private {function} - observer of the numberProperty. To be unlinked in the dispose method.
       this._numberPropertyObserver = this._updateNumberDisplay.bind( this );
       numberProperty.link( this._numberPropertyObserver );
+
+      // Apply any additionally Bounds setters
+      this.mutate( options );
     }
 
     /**
@@ -111,31 +113,19 @@ define( require => {
     }
 
     /**
-     * Formats a value to the displayed string value, by rounding and appending units if applicable.
-     * @public
-     *
-     * @param {number|null} - the value to format
-     * @returns {string} - formats the value
-     */
-    _formatValue( value ) {
-      assert( value === null || this._range.contains( value ), `value outside of range of NumberDisplay: ${ value }` );
-
-      if ( value === null ) return Symbols.NO_VALUE; // return the em-dash string if the value is null
-      return `${ this._decminalPlaces ? Util.toFixed( value, this._decminalPlaces ) } ${ this._unit || '' }`;
-    }
-
-    /**
      * Called when the slider needs to be updated, usually when the numberProperty of the NumberDisplay changes.
      * @private
      *
      * Updates the Text center location to match the alignments.
      */
-    _updateSlider() {
+    _updateNumberDisplay() {
       assert( this._numberProperty.value === null || this._range.contains( this._numberProperty.value ),
         `numberProperty outside of range of NumberDisplay range: ${ this._numberProperty.value }` );
+      const value = this._numberProperty.value; // convenience reference
 
       // Update the text of the value Node.
-      this._valueNode.text = this._formatValue( this._numberProperty.value );
+      this._valueNode.text = value === null ? Symbols.NO_VALUE : // use em-dash if null value
+        `${ this._decminalPlaces ? Util.toFixed( value, this._decminalPlaces ) : value } ${ this._unit || '' }`;
 
       // Ensure that the content fits inside the background
       this._background.topLeft = Vector.ZERO;
