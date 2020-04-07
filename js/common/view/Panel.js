fb55d4b2f3547f5d2a1e5b660c1be8ecb6d9233d
Brandon Li <brandon.li820@icloud.com>
2020-04-07 14:57:52 -0600

    #5 first try at Panel

---
+++ js/common/view/Panel.js
@@ -0,0 +1,126 @@
+// Copyright © 2020 Brandon Li. All rights reserved.
+
+/**
+ * A Panel, which displays a content Node with a rectangular background.
+ *
+ * The background rectangle's width and height will automatically update if the content Node's bounds changes.
+ * Panel will have a vast options API to customize the appearance and behavior of the Panel, including padding between
+ * the content and the background.
+ *
+ * Panel is sub-typed and used throughout the Control Panels of Rotational Motion.
+ *
+ * @author Brandon Li <brandon.li820@gmail.com>
+ */
+
+define( require => {
+  'use strict';
+
+  // modules
+  const assert = require( 'SIM_CORE/util/assert' );
+  const Node = require( 'SIM_CORE/scenery/Node' );
+  const Rectangle = require( 'SIM_CORE/scenery/Rectangle' );
+
+  class Panel extends Node {
+
+    /**
+     * @param {Node} content
+     * @param {Object} [options] - Various key-value pairs that control the appearance and behavior of the Panel. See
+     *                             the code where the options are set in the early portion of the constructor.
+     */
+    constructor( content, options ) {
+      assert( content instanceof Node, `invalid content: ${ content }` );
+      assert( !options || Object.getPrototypeOf( options ) === Object.prototype, `invalid options: ${ options }` );
+      assert( !options || !options.attributes, 'Panel sets attributes' );
+      assert( !options || !options.width, 'Panel sets width' );
+      assert( !options || !options.height, 'Panel sets height' );
+
+      options = {
+        fill: 'white',    // {string|Gradient} - the fill of the background rectangle
+        stroke: 'black',  // {string|Gradient} - the stroke of the background rectangle
+        cornerRadius: 10, // {number} - the corner radius of the background rectangle
+        strokeWidth: 1,   // {number} - the stroke-width of the background rectangle
+        xMargin: 5,       // {number} - the x-margin between the background rectangle and the content
+        yMargin: 5,       // {number} - the y-margin between the background rectangle and the content
+
+        // Rewrite options so that the passed-in options overrides the defaults.
+        ...options
+      };
+      super( options );
+
+      // @private {*} - see options declaration for documentation.
+      this._xMargin = options.xMargin;
+      this._yMargin = options.yMargin;
+      this._content = content;
+
+      // @private {Rectangle} - create the background rectangle. Width and height to be updated later.
+      this._background = new Rectangle( 0, 0, {
+        fill: options.fill,
+        stroke: options.stroke,
+        strokeWidth: options.strokeWidth,
+        cornerRadius: options.cornerRadius
+      } ) ;
+
+      // @private {boolean} - Indicates if we are in the process of updating the layout of the Panel. Used
+      //                      to reduce the number of _recomputeAncestorBounds calls while layouting.
+      this._isUpdatingLayout = false;
+
+      // At this point, call mutate to ensure that any location setters provided are correctly mutated and our
+      // properties are correctly set. in Node.mutate()
+      this.mutate( options );
+    }
+
+    /**
+     * Called when the layout of the Panel needs to be updated.
+     * @private
+     *
+     * NOTE: Will change the location of its children, and possibly the location of this Node's content.
+     */
+    _updateLayout() {
+      this._isUpdatingLayout = true; // Indicate that we are now updating our layout.
+
+      // Ensure that the content fits inside the background Path
+      this._background.topLeft = Vector.ZERO;
+
+      // Update the width and height of the background.
+      this._background.width = this._content.width + this._xMargin;
+      this._background.height = this._content.height + this._yMargin;
+
+      // Align the content to the center of the background.
+      this._content.center = this._background.center;
+
+      this._isUpdatingLayout = false; // Indicate that we are now done updating our layout of our children.
+      super._recomputeAncestorBounds();
+    }
+
+    /**
+     * @override
+     * Ensures that the Panel is formatted when children are added.
+     * @public
+     *
+     * @param {Node} child
+     * @returns {Node} - Returns 'this' reference, for chaining
+     */
+    addChild( child ) {
+      assert( child instanceof Node, `invalid child: ${ child }` );
+      super.addChild( child );
+
+      this._updateLayout();
+      return this;
+    }
+
+    /**
+     * @override
+     * Called when this Node's Bounds changes due to a child's Bounds changing or when a child is added or removed.
+     * Also responsible for recursively calling the method for each parent up to either the ScreenView or to the
+     * point where a Node doesn't have a parent.
+     * @protected
+     *
+     * This is overridden so that the layout of the Panel updates when a child changes.
+     */
+    _recomputeAncestorBounds() {
+      if ( this._isUpdatingLayout === false ) return this._updateLayout();
+    }
+  }
+
+  return Panel;
+} );
