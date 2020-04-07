ceae0021c54a98c391e39c30e065a3d0ee63f87e
Brandon Li <brandon.li820@icloud.com>
2020-04-06 22:52:02 -0600

    #5 start doc of NumberControlSet

--- js/common/view/NumberControlSet.js
+++ js/common/view/NumberControlSet.js
@@ -1,14 +1,21 @@
-// Copyright © 2019-2020 Brandon Li. All rights reserved.
+// Copyright © 2020 Brandon Li. All rights reserved.
 
 /**
- * A rotational-motion specific slider.
+ * Control Set for changing and displaying the value of a number Property.
  *
- * Includes:
- *   - A label above the slider.
- *   - A Rectangle and Text that shows the slider value. TODO: should this be generalized in sim-core?
- *   - A Slider Below
+ * A NumberControlSet consists of:
+ *   - A text label
+ *   - A NumberDisplay of the number Property. See NumberDisplay.js for doc.
+ *   - A Slider to modify the value of the number Property.
  *
- * @author Brandon Li
+ * The NumberControlSet looks visually like:
+ *
+ *  title    |number|
+ *   ─────▉─────────
+ *
+ * Used throughout Rotational Motion inside of the control panels.
+ *
+ * @author Brandon Li <brandon.li820@gmail.com>
  */
 
 define( require => {
@@ -16,138 +23,134 @@ define( require => {
 
   // modules
   const assert = require( 'SIM_CORE/util/assert' );
+  const FlexBox = require( 'SIM_CORE/scenery/FlexBox' );
+  const Node = require( 'SIM_CORE/scenery/Node' );
+  const NumberDisplay = require( 'ROTATIONAL_MOTION/common/view/NumberDisplay' );
   const Property = require( 'SIM_CORE/util/Property' );
+  const Range = require( 'SIM_CORE/util/Range' );
   const Rectangle = require( 'SIM_CORE/scenery/Rectangle' );
-  const SliderNode = require( 'SIM_CORE/scenery/SliderNode' );
-  const SVGNode = require( 'SIM_CORE/scenery/SVGNode' );
+  const Slider = require( 'SIM_CORE/scenery/Slider' );
   const Text = require( 'SIM_CORE/scenery/Text' );
   const Util = require( 'SIM_CORE/util/Util' );
   const Vector = require( 'SIM_CORE/util/Vector' );
 
-  class RotationalMotionSlider extends SVGNode {
+  class NumberControlSet extends Node {
 
     /**
-     * @param {Property.<number>} sliderProperty
-     * @param {Vector} sliderRange
-     * @param {Property.<boolean>} isPlayingProperty
-     * @param {Object} [options] - Various key-value pairs that control the appearance and behavior.
+     * @param {string} title - the text displayed in the label.
+     * @param {Property.<number|null>} numberProperty
+     * @param {Range} range - this range of the numberProperty.
+     * @param {Object} [options] - Various key-value pairs that control the appearance and behavior. See the code where
+     *                             the options are set in the early portion of the constructor for details.
      */
-    constructor( sliderProperty, sliderRange, isPlayingProperty, options ) {
-
-      assert( sliderProperty instanceof Property, `invalid sliderProperty: ${ sliderProperty }` );
-      assert( sliderRange instanceof Vector, `invalid sliderRange: ${ sliderRange }` );
-      assert( isPlayingProperty instanceof Property, `invalid isPlayingProperty: ${ isPlayingProperty }` );
+    constructor( title, numberProperty, range, options ) {
+      assert( typeof title === 'string', `invalid title: ${ title }` );
+      assert( numberProperty instanceof Property, `invalid numberProperty: ${ numberProperty }` );
+      assert( range instanceof Range, `invalid range: ${ range }` );
       assert( !options || Object.getPrototypeOf( options ) === Object.prototype, `invalid options: ${ options }` );
 
-      //----------------------------------------------------------------------------------------
-
       options = {
+        sliderTopMargin: 10, // {number} margin between the slider and the content above
 
-        width: 240, // required
+        // {Object} - if provided, these options will be passed to the Slider instance
+        sliderOptions: null,
 
-        //----------------------------------------------------------------------------------------
-        // specific to this class
+        // {Object} - if provided, these options will be passed to the Text instance for the title
+        titleTextOptions: null,
 
-        padding: 10, // eye-balled
-        sliderLabelMargin: 40, // margin between the slider and the content above
+        // {Object} - if provided, these options will be passed to the NumberDisplay instance
+        numberDisplayOptions: null,
 
-        sliderOptions: null, // passed to the slider
-        sliderTickIncrement: 0.1,
-
-        // label
-        labelFontSize: 15,
-        labelText: '',
-
-        fontWeight: '100',
-
-        // number display
-        numberDisplaySize: new Vector( 90, 27 ),
-        numberDisplayFontSize: 13,
-        numberDisplayDecimalPlaces: 2,
-        numberDisplayUnit: null,
-
-        // rewrite options such that it overrides the defaults above if provided.
+        // Rewrite options so that it overrides the defaults.
         ...options
       };
 
       super( options );
 
       //----------------------------------------------------------------------------------------
-      // Create The Drag listeners for when sliders are dragged to correctly pause and play the sim.
 
-      let playAtDragStart;
-      const startDrag = () => {
-        playAtDragStart = isPlayingProperty.value;
-        isPlayingProperty.value = false;
-      };
-      const endDrag = () => {
-        playAtDragStart && isPlayingProperty.toggle();
-        playAtDragStart = null;
-      };
+      // @private {*} - see parameter declaration for documentation. Referenced for use in our methods.
+      this._numberProperty = numberProperty;
+      this._range = range;
+      this._sliderTopMargin = options.sliderTopMargin;
 
-      //----------------------------------------------------------------------------------------
-      // Create the Number Display
-      const numberDisplay = new Rectangle( {
-        width: options.numberDisplaySize.x,
-        height: options.numberDisplaySize.y,
-        fill: 'white',
-        stroke: 'rgb( 150, 150, 150 )',
-        strokeWidth: 0.5,
-        x: options.width - options.padding - options.numberDisplaySize.x
-      } );
-      const numberDisplayText = new Text( {
-        fontSize: options.numberDisplayFontSize,
-        fontWeight: options.fontWeight,
-        x: numberDisplay.x + numberDisplay.width / 2,
-        y: numberDisplay.height / 2,
-        attributes: {
-          'text-anchor': 'middle',
-          'alignment-baseline': 'middle'
-        }
-      } );
+      // @private {Text} - the Text Node for the title label of the NumberControlSet
+      this._titleNode = new Text( title, options.titleTextOptions );
 
-      //----------------------------------------------------------------------------------------
-      // Create the label that labels the slider on the left
-      const label = new Text( {
-        text: options.labelText,
-        fontSize: options.labelFontSize,
-        fontWeight: options.fontWeight,
-        attributes: {
-          'text-anchor': 'start',
-          'alignment-baseline': 'baseline'
-        },
-        x: options.padding,
-        y: numberDisplay.height // align baseline with number display
-      } );
+      // @private {NumberDisplay} - the number display of the NumberControlSet
+      this._numberDisplay = new NumberDisplay( numberProperty, range, options.numberDisplayOptions );
 
-      //----------------------------------------------------------------------------------------
-      // Create the slider to change the Number Property
-      const slider = new SliderNode(
-        sliderRange,
-        sliderProperty, {
-          minorTickIncrement: options.sliderTickIncrement,
-          startDrag,
-          endDrag,
-          ...options.sliderOptions
-      } );
-      slider.x = options.width / 2 - slider.width / 2;
-      slider.y = numberDisplay.height + options.sliderLabelMargin;
+      // @private {Slider} (read-only) - the slider of the NumberControlSet.
+      this._slider = new Slider( range, numberProperty, options.sliderOptions );
 
-      //----------------------------------------------------------------------------------------
-      // Update the number display text when the Slider value changes
-      sliderProperty.link( value => {
-        numberDisplayText.setText(
-          `${ Util.toFixed( value, options.numberDisplayDecimalPlaces ) } ${ options.numberDisplayUnit }` );
-      } );
+      // Add the content of the NumberControlSet as children
+      this.children = [ this._titleNode, this._numberDisplay, this._slider ];
 
-      this.setChildren( [
-        numberDisplay,
-        numberDisplayText,
-        label,
-        slider
-        ] );
+      // Update the layout of our children.
+      this._updateLayout();
+
+      // Apply any additionally Bounds setters
+      this.mutate( options );
+    }
+
+    /**
+     * @override
+     * Disposes the NumberControlSet and its listeners so that it can be garbage collected.
+     * @public
+     */
+    dispose() {
+      this._slider.dispose();
+      this._numberDisplay.dispose();
+      this._titleNode.dispose();
+      super.dispose();
+    }
+
+    /**
+     * Adds a major tick mark to the slider.
+     * @public
+     *
+     * @param {number} value - the numeric value the tick represents along the range of the slider.
+     * @param {Node} [label] - optional label Node (usually a Text Node), placed above the Node.
+     * @returns {NumberControlSet} - 'this' reference, for chaining.
+     */
+    addSliderMajorTick( value, label ) {
+      this._slider.addMajorTick( value, label );
+      this._updateLayout();
+    }
+
+    /**
+     * Adds a minor tick mark to the slider.
+     * @public
+     *
+     * @param {number} value - the numeric value the tick represents along the range of the slider.
+     * @param {Node} [label] - optional label Node (usually a Text Node), placed above the Node.
+     * @returns {NumberControlSet} - 'this' reference, for chaining.
+     */
+    addSliderMinorTick( value, label ) {
+      this._slider.addMinorTick( value, label );
+      this._updateLayout();
+    }
+
+    /**
+     * Called when the layout of the NumbderControlSet needs to be updated.
+     * @private
+     *
+     * NOTE: Will change the location of its children, and possibly the location of this Node's content.
+     */
+    _updateLayout() {
+      this._titleNode.left = this._slider.left;
+      this._numberDisplay.right = this._slider.right;
+
+      if ( this._titleNode.height > this._numberDisplay.height ) {
+        this._numberDisplay.centerY = this._titleNode.centerY;
+        this._slider.top = this._titleNode.bottom + this._sliderTopMargin;
+      }
+      else {
+        this._titleNode.centerY = this._numberDisplay.centerY;
+        this._slider.top = this._numberDisplay.bottom + this._sliderTopMargin;
+      }
     }
   }
 
-  return RotationalMotionSlider;
+  return NumberControlSet;
 } );
