2cca566152ec4f72d75fc9314e81fa4459720c3c
Brandon Li <brandon.li820@icloud.com>
2020-04-09 16:42:15 -0600

    #5 add alignBox

---
+++ js/common/view/AlignBox.js
@@ -0,0 +1,101 @@
+// Copyright © 2020 Brandon Li. All rights reserved.
+
+/**
+ * A custom AlignBox, which aligns a content Node within a fixed width and height.
+ *
+ * Currently across different icons and Text Nodes that need to be the same dimensions for visual alignment inside of
+ * RadioButtons or Panels.
+ *
+ * AlignBox does not currently support dynamic content bounds.
+ *
+ * @author Brandon Li <brandon.li820@gmail.com>
+ */
+
+define( require => {
+  'use strict';
+
+  // modules
+  const assert = require( 'SIM_CORE/util/assert' );
+  const Node = require( 'SIM_CORE/scenery/Node' );
+
+  class AlignBox extends Node {
+
+    /**
+     * @param {Node} content - Node wrapped by the AlignBox
+     * @param {number} fixedWidth - the fixed width of the AlignBox
+     * @param {number} fixedHeight - the fixed height of the AlignBox
+     * @param {Object} [options] - Various key-value pairs that control the appearance and behavior. See the code where
+     *                             the options are set in the early portion of the constructor for details.
+     */
+    constructor( content, fixedWidth, fixedHeight, options ) {
+      assert( content instanceof Node, `invalid content: ${ content }` );
+      assert( typeof fixedWidth === 'number', `invalid fixedWidth: ${ fixedWidth }` );
+      assert( typeof fixedHeight === 'number', `invalid fixedHeight: ${ fixedHeight }` );
+      assert( !options || Object.getPrototypeOf( options ) === Object.prototype, `invalid options: ${ options }` );
+
+      options = {
+
+        // {string} - determines how the content is aligned relative to the fixed bounds: 'left', 'center', or 'right'.
+        xAlign: 'center',
+
+        // {string} - determines how the content is aligned relative to the fixed bounds: 'top', 'center', or 'bottom'.
+        yAlign: 'center',
+
+        // Rewrite options so that it overrides the defaults.
+        ...options
+      };
+
+      super( options );
+
+      //----------------------------------------------------------------------------------------
+
+      // Add a spacer to align ensure that the Bounds of the AlignBox is fixed.
+      const spacer = new Node( { width: fixedWidth, height: fixedHeight } );
+      content.mutate( { maxWidth: fixedWidth, maxHeight: fixedHeight } );
+
+      // Strip center to centerX or centerY
+      const xAlignKey = options.xAlign === 'center' ? 'centerX' : options.xAlign;
+      const yAlignKey = options.yAlign === 'center' ? 'centerY' : options.yAlign;
+
+      // Align the content relative to the spacer
+      content[ xAlignKey ] = spacer[ xAlignKey ];
+      content[ yAlignKey ] = spacer[ yAlignKey ];
+
+      // Set the children of the AlignBox. Use super.addChild as our addChild method is blocked off.
+      super.addChild( spacer );
+      super.addChild( content );
+
+      // Set the width and height of this Node
+      this.width = fixedWidth;
+      this.height = fixedHeight;
+
+      // Apply any additional Bounds mutators
+      this.mutate( options );
+    }
+
+    /**
+     * @override
+     * Ensures that all addChild isn't called on a AlignBox.
+     * @override
+     * @public
+     *
+     * @param {Node} child
+     */
+    addChild( child ) { assert( false, 'cannot addChild of a AlignBox' ); }
+
+    /**
+     * @override
+     * This method is called when a child's Bounds changes. In Node, this method is responsible for adjusting its
+     * Bounds and recursively calling the method for each parent up the ancestor tree.
+     * @protected
+     *
+     * Since the width and the height of this Bounds never changes, this function is overridden such that it only
+     * forwards the method call to the parent.
+     */
+    _recomputeAncestorBounds() {
+      if ( this.parent instanceof Node ) this.parent._recomputeAncestorBounds();
+    }
+  }
+
+  return AlignBox;
+} );
--- js/common/view/RotationalMotionIconFactory.js
+++ js/common/view/RotationalMotionIconFactory.js
@@ -1,9 +1,14 @@
 // Copyright © 2020 Brandon Li. All rights reserved.
 
 /**
- * Factory for creating the various icons that appear in the sim.
+ * Factory for creating the various icons that appear in the 'Rotational Motion' simulation.
  *
- * All floating numbers in this file were determined empirically and are tentative.
+ * ## Creates the following icons:
+ *  1. Screen icons (TODO)
+ *  2. Checkbox icons (i.e. arrow icons, etc.)
+ *  3. CircularMotionTypesIcon (on the 'Intro' screen)
+ *
+ * IMPORTANT: All floating numbers in this file were determined empirically and are tentative.
  *
  * @author Brandon Li <brandon.li820@gmail.com>
  */
@@ -12,6 +17,7 @@ define( require => {
   'use strict';
 
   // modules
+  const AlignBox = require( 'ROTATIONAL_MOTION/common/view/AlignBox' );
   const Arrow = require( 'SIM_CORE/scenery/Arrow' );
   const CircularMotionTypes = require( 'ROTATIONAL_MOTION/intro/model/CircularMotionTypes' );
   const Node = require( 'SIM_CORE/scenery/Node' );
@@ -38,23 +44,15 @@ define( require => {
     },
 
     /**
-     * Creates the icon on the CircularMotion types radio buttons in the 'intro' screen.
+     * Creates the icon on the CircularMotion types radio buttons in the 'Intro' screen.
      * @public
      *
      * @param {Enum.Member.<CircularMotionTypes>} circularMotionType
      * @returns {Node}
      */
     createCircularMotionTypeIcon( circularMotionType ) {
-      const text = new Text( circularMotionType === CircularMotionTypes.UNIFORM ? 'Uniform' : 'Non-uniform' );
-
-      const fixedWidthNode = new Node();
-      fixedWidthNode.width = 100; // eye-balled
-      text.centerY = fixedWidthNode.centerY;
-      text.centerX = fixedWidthNode.centerX;
-
-      const icon = new Node().setChildren( [ fixedWidthNode, text ] );
-      icon.width = fixedWidthNode.width;
-      return icon;
+      const label = new Text( circularMotionType === CircularMotionTypes.UNIFORM ? 'Uniform' : 'Non-uniform' );
+      return new AlignBox( label, 100, 50 );
     }
   };
 
