9cd4c1d82716f03ff20059f1666f061de5078670
Brandon Li <brandon.li820@icloud.com>
2020-04-11 22:38:39 -0600

    Make label optional string

--- js/common/view/VisibilityCheckbox.js
+++ js/common/view/VisibilityCheckbox.js
@@ -2,7 +2,7 @@
 
 /**
  * VisibilityCheckbox is a FlexBox sub-type that displays content to the right side of a Checkbox that toggles the
- * visibility of a simulation component.
+ * visibility of a simulation component. They are meant to be placed inside of Panels.
  *
  * The VisibilityCheckbox looks visually like:
  *
@@ -20,21 +20,23 @@ define( require => {
   // modules
   const assert = require( 'SIM_CORE/util/assert' );
   const Checkbox = require( 'SIM_CORE/scenery/components/buttons/Checkbox' );
-  const Node = require( 'SIM_CORE/scenery/Node' );
   const FlexBox = require( 'SIM_CORE/scenery/FlexBox' );
+  const Node = require( 'SIM_CORE/scenery/Node' );
+  const RotationalMotionConstants = require( 'ROTATIONAL_MOTION/common/RotationalMotionConstants' );
+  const Text = require( 'SIM_CORE/scenery/Text' );
 
   class VisibilityCheckbox extends FlexBox {
 
     /**
      * @param {Property.<boolean>} visibilityProperty - the Property to toggle when the Checkbox is pressed. The initial
      *                                                  visibility of the check will be determined by the current value.
-     * @param {Node} label - the label Node of the VisibilityCheckbox, placed to the right of the Checkbox
+     * @param {string} [label] - the label Text of the VisibilityCheckbox, placed to the right of the Checkbox
      * @param {Node} [icon] - the icon of the VisibilityCheckbox, placed to the right of the label
      * @param {Object} [options] - Various key-value pairs that control the appearance and behavior. See the code where
      *                             the options are set in the early portion of the constructor for details.
      */
     constructor( visibilityProperty, label, icon, options ) {
-      assert( label instanceof Node, `invalid label: ${ label }` );
+      assert( !label || typeof label === 'string', `invalid label: ${ label }` );
       assert( !icon || icon instanceof Node, `invalid icon: ${ icon }` );
       assert( !options || Object.getPrototypeOf( options ) === Object.prototype, `invalid options: ${ options }` );
 
@@ -55,11 +57,12 @@ define( require => {
 
       //----------------------------------------------------------------------------------------
 
-      options.children = [
-        new Checkbox( visibilityProperty, options.checkboxOptions ),
-        label,
-        icon || new Node()
-      ];
+      const children = [ new Checkbox( visibilityProperty, options.checkboxOptions ) ];
+
+      if ( label ) children.push( new Text( label, RotationalMotionConstants.PANEL_TEXT_OPTIONS ) );
+      if ( icon ) children.push( icon );
+
+      options.children = children;
 
       super( options );
     }
