b81ffb718f1f277f52cfe00d361e23be88555ec7
Brandon Li <brandon.li820@icloud.com>
2020-04-22 21:07:22 -0600

    Add Bounds of the window

--- js/rolling/view/RollingScreenView.js
+++ js/rolling/view/RollingScreenView.js
@@ -13,6 +13,7 @@ define( require => {
 
   // modules
   const assert = require( 'SIM_CORE/util/assert' );
+  const Bounds = require( 'SIM_CORE/util/Bounds' );
   const RollingModel = require( 'ROTATIONAL_MOTION/rolling/model/RollingModel' );
   const ScreenView = require( 'SIM_CORE/scenery/ScreenView' );
 
@@ -25,6 +26,28 @@ define( require => {
       assert( rollingModel instanceof RollingModel, `invalid rollingModel: ${ rollingModel }` );
 
       super();
+
+      //----------------------------------------------------------------------------------------
+
+      // @public (read-only) {Bounds} - The Bounds of the entire browser window (excluding the navigation-bar), in
+      //                                scenery coordinates, relative to our layout bounds (can include negative Bounds)
+      this.windowSceneryBounds = Bounds.ZERO.copy();
+    }
+
+    /**
+     * Layouts the Rolling screen. Called at the start of the simulation and when the browser window is resized.
+     * @public
+     *
+     * @param {number} width - Screen width, in pixels.
+     * @param {number} height - Screen height, in pixels.
+     */
+    layout( width, height ) {
+      super.layout( width, height );
+
+      // Recompute our window bounds.
+      const xExpand = ( width / this.layoutScale ) / 2;
+      const yExpand = ( height / this.layoutScale ) / 2;
+      this.windowSceneryBounds.set( this.layoutBounds ).expand( xExpand, yExpand, xExpand, yExpand );
     }
   }
 
