5fb96c3bf17a18c605c88bd329633c40aa83fe6b
Brandon Li <brandon.li820@icloud.com>
2020-04-30 23:18:04 -0600

    skeleton of Ramp Angle Node

---
+++ js/rolling/view/RampAngleNode.js
@@ -0,0 +1,16 @@
+// Copyright © 2020 Brandon Li. All rights reserved.
+
+/**
+ *
+ * @author Brandon Li <brandon.li820@gmail.com>
+ */
+
+define( require => {
+  'use strict';
+
+  class RampAngleNode extends Node {
+
+  }
+
+  return RampAngleNode;
+} );
